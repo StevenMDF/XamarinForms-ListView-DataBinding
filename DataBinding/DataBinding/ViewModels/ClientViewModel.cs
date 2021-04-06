936afe76b43e51032b67a1ea317c06933038f3ba
Daniel Tibbotts <dtibbotts@outlook.com>
2021-04-06 21:34:39 +1200

    Created Test App

---
+++ DataBinding/DataBinding/ViewModels/ClientViewModel.cs
@@ -0,0 +1,76 @@
+﻿using DataBinding.Models;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace DataBinding.ViewModels
+{
+    public class ClientViewModel : BaseViewModel
+    {
+        private ObservableCollection<Client> clients;
+        public ObservableCollection<Client> Clients 
+        {
+            get { return clients; } 
+            set
+            {
+                clients = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Command SaveClientCommand { get; }
+
+        public ClientViewModel()
+        {
+            this.Clients = new ObservableCollection<Client>();
+
+            SaveClientCommand = new Command(()=> {
+
+                Client client = new Client()
+                {
+                    Name = Name,
+                    Phone = Phone
+                };
+
+                Clients.Add(client);
+                OnPropertyChanged(nameof(Clients));
+            });
+
+
+
+        }
+
+        private int id;
+        public int Id 
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string phone;
+        public string Phone
+        {
+            get { return phone; }
+            set 
+            {
+                phone = value;
+                OnPropertyChanged();
+            }
+        }
+
+    }
+}
